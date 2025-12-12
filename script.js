42a32ecc72c667911ca75fcf7a1e1a2b4f887c4f
FMalfonso <163765926+fmAlfonso@users.noreply.github.com>
2025-12-12 14:39:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ let btnProcess = document.querySelector("#btnProcess");
 
 btnProcess.addEventListener("click", function() {
     let sentence = txtInput.value;
-    let outputSentence = sentence.includes("JavaScript is easy") ? "Found" : "Not Found";
+    let outputSentence = sentence.includes("easy") ? "Found" : "Not Found";
     output.innerHTML = outputSentence;
 });
